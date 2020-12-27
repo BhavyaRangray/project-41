d488477e610c30bac74d1b48a7a6b5c6fd71c6c4
BhavyaRangray <72501879+BhavyaRangray@users.noreply.github.com>
2020-12-27 16:41:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function preload(){
+    images=sprites.loadImage=sprites;
+    }
+function setup(){
+}
+function draw(){
+    sprites.display();
+
+}   
+var png = (100,279,50,50);
+var animation = add 
+
